ddb9b6380c0aa5c91fc0ad54f9afc71fe18cf202
heronhoga <nozzeravixer@gmail.com>
2024-11-10 09:30:49 +0700

    fix:fix state for aerator

--- app/[location]/page.js
+++ app/[location]/page.js
@@ -1,19 +1,39 @@
-'use client'
+'use client';
 
 import { useParams } from 'next/navigation';
 import useWebSocket from '@/app/hooks/useWebSocket';
+import { useEffect, useState } from 'react';
 
 const Home = () => {
     const { location } = useParams();
     const url = `ws://iot.crustea.id:1880/${location}/control`;
+    const relayStateUrl = `http://iot.crustea.id:1880/${location}/control/state`;
     const { messages, sendMessage } = useWebSocket(url);
+    
+    const [relayStatus, setRelayStatus] = useState('Unknown');
+
+    useEffect(() => {
+        const fetchRelayStatus = async () => {
+            try {
+                const response = await fetch(relayStateUrl);
+                const data = await response.json();
+                setRelayStatus(data.state === 'open_relay' ? 'ON' : 'OFF');
+            } catch (error) {
+                console.error('Error fetching relay status:', error);
+            }
+        };
+
+        fetchRelayStatus();
+    }, [location, relayStateUrl]);
 
     const handleOpenRelay = () => {
         sendMessage('open_relay');
+        setRelayStatus('ON');
     };
 
     const handleCloseRelay = () => {
         sendMessage('close_relay');
+        setRelayStatus('OFF');
     };
 
     return (
@@ -21,6 +41,9 @@ const Home = () => {
             <h1 className="text-4xl sm:text-5xl font-extrabold text-white mb-10 text-center">
                 Aerator Control for {location}
             </h1>
+            <p className="text-xl sm:text-2xl text-white mb-6">
+                Current Relay Status: <span className="font-bold">{relayStatus}</span>
+            </p>
             <div className="flex flex-wrap justify-center space-x-0 sm:space-x-6 space-y-4 sm:space-y-0 mb-10">
                 <button
                     onClick={handleOpenRelay}
